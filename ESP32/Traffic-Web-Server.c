b7f09d330b7d3893a6e388731abb386088930380
Gemiey <45759725+Gemiey@users.noreply.github.com>
2021-11-27 10:33:49 +0200

    Update Traffic-Web-Server.c

--- ESP32/Traffic-Web-Server.c
+++ ESP32/Traffic-Web-Server.c
@@ -47,7 +47,7 @@ void setup() {
   server.begin();
 }
 
-void loop(){
+void loop(){ // when receives emergency signal enters loops to check for coordinates
   WiFiClient client = server.available();   // Listen for incoming clients
 
   if (client) {                             // If a new client connects,
